0c86d5a4e46d67f65c2add2f5bb102837e0f116f
Gordon Mo <gmo30@stuy.edu>
2022-11-09 16:59:17 -0500

    added

--- app/db_builder.py
+++ app/db_builder.py
@@ -5,18 +5,18 @@ Softdev
 '''
 
 import sqlite3
+import os
 
 DB_FILE = "discobandit.db"
 
 db = sqlite3.connect(DB_FILE)
-c = db.cursor
-
-c.executescript("""
-create table user(userID int primary key, username text, password text);
-create table story(storyID int primary key, title text, content text, recent text);
-create table links(page text, url text);
-create table contribution(userID int primary key, storyID int primary key);
-""")
+c = db.cursor()
+if not(os.path.exists(DB_FILE)):  
+        c.executescript("""create table user(user_id int primary key, username text, password text);
+        create table story(story_id int primary key, title text, content text, recent text);
+        create table links(page text, url text);
+        create table contribution(user_id int, story_id int);
+        """)
 
 db.commit() #save changes
 db.close()  #close database
