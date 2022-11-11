e6387c37a78cbd9412716932f479ab10093d0d0b
Joshua Liu <jliu31@stuy.edu>
2022-11-10 22:09:23 -0500

    register is now working

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ SoftDev
 '''
 
 from flask import Flask, session, render_template, request, redirect, url_for
-from db import User_pass_match, User_exists
+from db import User_pass_match, User_exists, Add_user
 
 app = Flask(__name__)
 
@@ -16,7 +16,9 @@ def login_page():
     else:   # This checks for the login info
         if(User_exists(request.form.get("username"))):
             if(User_pass_match(request.form.get("username"), request.form.get("password"))):
+                # session stuff here
                 return redirect(url_for("home_page"))
+        # these are for failed logins
             return render_template("login.html", response="The password you entered does not match the username.") # displays responses based on error
         return render_template("login.html", response="The username you entered does not exist.")
 
@@ -25,13 +27,16 @@ def login_page():
 def register():
     if(request.method == "GET"):
         return render_template("register.html")
-    # else:
-    #     if(not User_exists(request.form.get("username"))):
-    #         if(request.form.get("Confirm Password") == request.form.get("password")):
-    #             #Add_user(request.form.get("username"), request.form.get("password"))
-    #             return redirect(url_for("home_page"))
-    #         return render_template("register.html")#if passwords dont match
-    #     return render_template("register.html")
+    else:
+        if(not User_exists(request.form.get("username"))):
+            print(request.form.get("Password_confirm") + "   " + request.form.get("password"))
+            if(request.form.get("Password_confirm") == request.form.get("password")):
+                Add_user(request.form.get("username"), request.form.get("password"))
+                #session Stuff here
+                return redirect(url_for("home_page"))
+        # Failed register responses:
+            return render_template("register.html", response="The passwords you entered do not match")
+        return render_template("register.html",response="Username already exists")
 
 @app.route("/home", methods=["GET", "POST"])
 def home_page():
